304b8ab5fa48434b5c36ee0813967ef5c116cd6b
unknown <2100902@uad.ac.uk>
2023-11-06 02:41:38 +0000

    added buttons for add, delete and update assets

--- ScottishGlen/cmp307/cmp307/Form1.Designer.cs
+++ ScottishGlen/cmp307/cmp307/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AddAsset = new System.Windows.Forms.Button();
+            this.UpdateAsset = new System.Windows.Forms.Button();
+            this.DeleteAsset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddAsset
+            // 
+            this.AddAsset.Location = new System.Drawing.Point(15, 14);
+            this.AddAsset.Name = "AddAsset";
+            this.AddAsset.Size = new System.Drawing.Size(140, 80);
+            this.AddAsset.TabIndex = 0;
+            this.AddAsset.Text = "Add Asset";
+            this.AddAsset.UseVisualStyleBackColor = true;
+            // 
+            // UpdateAsset
+            // 
+            this.UpdateAsset.Location = new System.Drawing.Point(15, 100);
+            this.UpdateAsset.Name = "UpdateAsset";
+            this.UpdateAsset.Size = new System.Drawing.Size(140, 80);
+            this.UpdateAsset.TabIndex = 2;
+            this.UpdateAsset.Text = "Update Asset";
+            this.UpdateAsset.UseVisualStyleBackColor = true;
+            // 
+            // DeleteAsset
+            // 
+            this.DeleteAsset.Location = new System.Drawing.Point(161, 14);
+            this.DeleteAsset.Name = "DeleteAsset";
+            this.DeleteAsset.Size = new System.Drawing.Size(140, 80);
+            this.DeleteAsset.TabIndex = 3;
+            this.DeleteAsset.Text = "Delete Asset";
+            this.DeleteAsset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteAsset);
+            this.Controls.Add(this.UpdateAsset);
+            this.Controls.Add(this.AddAsset);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AddAsset;
+        private System.Windows.Forms.Button UpdateAsset;
+        private System.Windows.Forms.Button DeleteAsset;
     }
 }
 
